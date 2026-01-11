b89b7ae28f7d7894f514db4274b0d4428d0b4dcd
Hanna Seithe <hannaseithe@googlemail.com>
2026-01-11 13:50:32 +0100

    Clean up models.py

--- cats/models.py
+++ cats/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-from django.db.models import Q, CheckConstraint, F, UniqueConstraint
-from django.core.validators import MinValueValidator, MaxValueValidator
-from django.core.exceptions import ValidationError
 
 
 
