a33f08dcd61b2c9c85d60a230cf18a15f1dab40b
Hanna Seithe <hannaseithe@googlemail.com>
2026-02-05 17:10:13 +0100

    Add one more test

--- cats/tests/test_filters.py
+++ cats/tests/test_filters.py
@@ -46,3 +46,28 @@ def test_filter_by_created_before(auth_client_with_refresh, create_user, create_
     assert response2.status_code == 200, "response status code not 200"
     sims = response2.data
     assert len(sims) == 0, "Length of response list is not 0"
+
+def test_filter_by_created_after(auth_client_with_refresh, create_user, create_simulation_list):
+    before_creation = datetime.now()
+    before_creation_iso = before_creation.isoformat()
+
+    user = create_user(email="test1@email.com",password="test1password")
+    sim_list = create_simulation_list(user = user)
+
+    after_creation = datetime.now()
+    after_creation_iso = after_creation.isoformat()
+
+    auth_client, _ = auth_client_with_refresh(user=user, password="test1password")
+
+    url = reverse("simulation-list")
+    response1 = auth_client.get(url, {"created_after": before_creation_iso})
+    
+    assert response1.status_code == 200, "response status code not 200"
+    sims = response1.data
+    assert len(sims) == 5, "Length of response list is not 5"
+    assert sims[0] == SimulationStatusSerializer(sim_list[0]).data, "First Simulation of List response is different from first simulation of list in memory"
+
+    response2 = auth_client.get(url, {"created_after": after_creation_iso})
+    assert response2.status_code == 200, "response status code not 200"
+    sims = response2.data
+    assert len(sims) == 0, "Length of response list is not 0"
